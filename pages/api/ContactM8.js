248bd73f1badf69e82371703be2d72604c60ad97
m8software <akser360@yahoo.com>
2021-04-23 14:20:48 -0400

    email api added cors

--- pages/api/ContactM8.js
+++ pages/api/ContactM8.js
@@ -1,9 +1,21 @@
 // require("dotenv").config();
 const nodemailer = require("nodemailer");
 
+import Cors from 'cors'
 
-export default (req, res) => {
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+)
 
+
+export default async function handler(req, res) {
+  // Run cors
+  await cors(req, res)
   console.log(req.body)
 
     const { name, email, company, text } = req.body;
